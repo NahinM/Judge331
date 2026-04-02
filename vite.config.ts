d5b6ed5527e6729be460983f78376e9ba90937b5
Nahin Munkar <nahinmunkarnm@gmail.com>
2026-04-02 11:30:27 +0600

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/judge331/',
+  base:'/Judge331/',
   plugins: [
     react(),
     tailwindcss()
